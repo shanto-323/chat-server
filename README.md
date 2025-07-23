c1286979b3b1ec56df12450e451db4bdb5e0aa1b
SHANTO323 <shantochando323@gmail.com>
2025-07-23 12:28:45 +0600

    feat: compose for ui

--- README.md
+++ README.md
@@ -1,12 +1,11 @@
 # WebSocket Chat App with Fyne GUI
 A real-time chat application built with:
 - **Go** for the backend
-- **Fyne** for cross-platform GUI
+- **Compose** for cross-platform GUI
 - **WebSockets** for live messaging
 
 </br></br>
 ### INITIAL UI
 <p align="center">
-  <img src="asset/connect.png" width="45%">
-  <img src="asset/disconnect.png" width="45%">
+  <img src="asset/ui.png" width="70%">
 </p>
--- compose/composeApp/src/desktopMain/kotlin/com/shanto/chat_app/ui/screen/Screen.kt
+++ compose/composeApp/src/desktopMain/kotlin/com/shanto/chat_app/ui/screen/Screen.kt
@@ -4,20 +4,25 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.Button
-import androidx.compose.material.CircularProgressIndicator
-import androidx.compose.material.Icon
-import androidx.compose.material.Text
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.shanto.chat_app.model.ActivePool
 import com.shanto.chat_app.model.Client
 import kotlinx.coroutines.delay
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.json.decodeFromJsonElement
+import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
 fun Screen(
@@ -27,6 +32,7 @@ fun Screen(
     val response = viewModel.response.collectAsState().value
 
     var open by remember { mutableStateOf(false) }
+    var message by remember { mutableStateOf("") }
 
     LaunchedEffect(Unit) {
         while (true) {
@@ -57,46 +63,114 @@ fun Screen(
         }
 
         Event.Success -> {
-            Row(
+            Column(
                 modifier = Modifier
-                    .fillMaxSize()
-                    .background(Color.White),
-                verticalAlignment = Alignment.Top,
+                    .fillMaxSize(),
+                verticalArrangement = Arrangement.Top
             ) {
-                LazyColumn(
+
+                Row(
                     modifier = Modifier
-                        .fillMaxHeight()
-                        .fillMaxWidth(0.3F)
-                        .padding(10.dp),
-                    horizontalAlignment = Alignment.Start,
-                    verticalArrangement = Arrangement.Top
+                        .fillMaxWidth()
+                        .fillMaxHeight(0.1F)
+                        .padding(2.dp)
+                        .background(Color.Gray),
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.spacedBy(2.dp)
                 ) {
-                    items(state.ActivePool) {
-                        UserCard(
-                            onClick = {
-                                viewModel.ReceiverIdChanged(
-                                    id = it
-                                )
-                                open = true
-                            },
-                            id = it
-                        )
+                    Box(
+                        modifier = Modifier
+                            .fillMaxHeight()
+                            .weight(1f),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Text("CHAT APP")
                     }
+                    Text(
+                        text = "Icon",
+                        modifier = Modifier
+                            .fillMaxHeight()
+                            .aspectRatio(1f),
+                        style = TextStyle(
+                            textAlign = TextAlign.Center
+                        )
+                    )
+                    Text(
+                        text = "Icon",
+                        modifier = Modifier
+                            .fillMaxHeight()
+                            .aspectRatio(1f),
+                        style = TextStyle(
+                            textAlign = TextAlign.Center
+                        )
+                    )
                 }
 
-                ChatCard(
-                    id = state.ReceiverId,
+                Row(
                     modifier = Modifier
-                        .fillMaxHeight()
-                        .weight(1f),
-                    onClick = {
-                        viewModel.ReceiverIdChanged(
-                            id = null
-                        )
-                        open = false
-                    },
-                    open = open
-                )
+                        .fillMaxWidth()
+                        .weight(1f)
+                        .padding(2.dp)
+                        .background(Color.White),
+                    verticalAlignment = Alignment.Top,
+                ) {
+                    // Active List...
+                    Column(
+                        modifier = Modifier
+                            .fillMaxHeight()
+                            .fillMaxWidth(0.3F)
+                    ) {
+                        Box(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .fillMaxHeight(0.05F)
+                                .background(Color.Gray),
+                            contentAlignment = Alignment.Center
+                        ) {
+                            Text("ACTIVE LIST")
+                        }
+                        LazyColumn(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .weight(1f)
+                                .padding(horizontal = 5.dp, vertical = 0.dp),
+                            horizontalAlignment = Alignment.Start,
+                            verticalArrangement = Arrangement.Top
+                        ) {
+                            items(state.ActivePool) {
+                                UserCard(
+                                    onClick = {
+                                        viewModel.ReceiverIdChanged(
+                                            id = it
+                                        )
+                                        open = true
+                                    },
+                                    id = it,
+                                )
+                            }
+                        }
+                    }
+
+                    // Chat Box
+                    ChatCard(
+                        id = state.ReceiverId,
+                        modifier = Modifier
+                            .fillMaxHeight()
+                            .weight(1f),
+                        onClick = {
+                            viewModel.ReceiverIdChanged(
+                                id = null
+                            )
+                            open = false
+                        },
+                        open = open,
+                        onValueChange = {
+                            message = it
+                        },
+                        message = message
+                    )
+
+                }
 
             }
         }
@@ -113,10 +187,20 @@ fun UserCard(
         onClick = onClick,
         modifier = Modifier
             .fillMaxWidth()
-            .aspectRatio(2f)
-            .padding(5.dp)
+            .aspectRatio(4f)
+            .padding(5.dp),
+        colors = ButtonDefaults.buttonColors(
+            backgroundColor = Color.Black,
+            contentColor = Color.White
+        )
     ) {
-        Text(id)
+        Text(
+            text = id,
+            textAlign = TextAlign.Center,
+            fontSize = 10.sp,
+            fontWeight = FontWeight.Bold,
+            maxLines = 1
+        )
     }
 }
 
@@ -125,38 +209,114 @@ fun ChatCard(
     modifier: Modifier = Modifier,
     id: String?,
     onClick: () -> Unit = {},
-    open :Boolean
+    open: Boolean,
+    onValueChange: (String) -> Unit = {},
+    message : String
 ) {
-    LazyColumn(
+    Column(
         modifier = Modifier
             .then(modifier)
-            .padding(5.dp)
-            .background(Color.Gray),
-        horizontalAlignment = Alignment.Start,
-        verticalArrangement = Arrangement.Top
+            .padding(10.dp)
+            .background(Color.White),
     ) {
         if (open && id != null) {
-            item {
-                Row(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .background(Color.White),
-                    verticalAlignment = Alignment.CenterVertically,
-                    horizontalArrangement = Arrangement.End
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(32.dp),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.Center
+            ) {
+                Text(
+                    text = id,
+                    modifier = Modifier.padding(end = 2.dp).fillMaxHeight().weight(1f)
+                )
+                Button(
+                    onClick = { onClick.invoke() },
+                    modifier = Modifier.padding(start = 2.dp).fillMaxHeight().aspectRatio(1f),
+                    colors = ButtonDefaults.buttonColors(
+                        backgroundColor = Color.Black,
+                        contentColor = Color.White
+                    )
                 ) {
-                    Text(id)
-                    Button(
-                        onClick = {
-                            onClick.invoke()
-                        }
-                    ) {
-                        Text("close")
-                    }
+                    Text("Close")
                 }
             }
-            items(20) {
-                Text("message")
+
+            LazyColumn(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .weight(1f),
+                horizontalAlignment = Alignment.Start,
+                verticalArrangement = Arrangement.Bottom
+            ) {
+                items(20) {
+                    MessageCard("Message!!")
+                }
+            }
+
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(50.dp),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.Center
+            ) {
+                OutlinedTextField(
+                    value = message,
+                    onValueChange = onValueChange,
+                    modifier = Modifier.weight(1f),
+                    placeholder = {
+                        Text(
+                            text = "write something....",
+                            modifier = Modifier
+                                .fillMaxSize(),
+                            style = TextStyle(
+                                textAlign = TextAlign.Start,
+                                fontSize = 12.sp
+                            )
+                        )
+                    },
+                )
+                Button(
+                    onClick = {},
+                    modifier = Modifier.padding(start = 2.dp).fillMaxHeight().aspectRatio(1f),
+                    colors = ButtonDefaults.buttonColors(
+                        backgroundColor = Color.Black,
+                        contentColor = Color.White
+                    )
+                ) {
+                    Text(
+                        text = "Sned",
+                        modifier = Modifier
+                            .fillMaxSize(),
+                        style = TextStyle(
+                            textAlign = TextAlign.Center,
+                            fontSize = 12.sp
+                        )
+                    )
+                }
             }
         }
+    }
+}
+
+@Composable
+fun MessageCard(
+    text: String,
+    color: Color = Color.Gray
+) {
+    Box(
+        modifier = Modifier.padding(2.dp).clip(RoundedCornerShape(4.dp)).padding(2.dp).background(color),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = text,
+            textAlign = TextAlign.Center,
+            color = Color.Black,
+            fontSize = 12.sp,
+            fontWeight = FontWeight.SemiBold,
+            modifier = Modifier.padding(5.dp)
+        )
     }
 }
